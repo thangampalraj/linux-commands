5a7efa427d4b801a4d3320f663028789709fd4a8
thangampalraj <136328968+thangampalraj@users.noreply.github.com>
2023-07-10 15:32:47 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "*******Displaying server hostname********"
+hostname
+
+echo "*********print current working directory*******"
+pwd
+
+echo "**********display diskspace output***********"
+df -h
+
+echo "***********current user***********"
+whoami
