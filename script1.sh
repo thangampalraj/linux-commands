39fcbc252b33beb36331bfd83643763e8272dd26
thangampalraj <136328968+thangampalraj@users.noreply.github.com>
2023-07-27 12:23:07 +0100

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 password1=$1
 password2=$2
+change_number=$3
 echo "*******Displaying server hostname********"
 hostname
 
@@ -19,11 +20,20 @@ echo $password1
 echo ********secret2**********
 echo $password2
 
+echo *****changenumber*****
+echo $change_number
+
 echo ******change directory*********
 pwd
 cd /actions-runner/_work/linux-commands/linux-commands
 pwd
 ls
+echo *****backup****
+cp /apps/admin/sql/test.pks /apps/admin/sql/test_$change_number.pks
+cp /apps/admin/sql/test.pkb /apps/admin/sql/test_$change_number.pkb
+
 echo *****copy files
 rsync -avzh *.sh  /tmp/thangam
+rsync -avzh test.pks /apps/admin/sql/
+rsync -avzh test.pkb /apps/admin/sql
 
